9cc873a1a34c5055514379d401c4aa394c92e2e0
akh-seco <alejandro.kuratomi_hernandez@secotools.com>
2026-03-27 15:31:08 +0100

    data_extraction and data_processing .py files written

---
+++ src/data/data_extraction.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import logging
+
+class DataExtractor:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+    
+    def extract_data(self):
+        try:
+            data = pd.read_csv(self.file_path)
+            logging.info(f"Data successfully extracted from {self.file_path}")
+            return data
+        except Exception as e:
+            logging.error(f"Error extracting data from {self.file_path}: {e}")
---
+++ src/data/data_processing.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import numpy as np
+import logging
+
+class DataProcessor:
+    def __init__(self, data: pd.DataFrame):
+        self.data = data
+    
+    def rename_columns(self):
+        try:
+            if 'ProductNumber' in self.data.columns():
+                self.data.rename(columns={'ProductNumber': 'ItemNumber'},inplace=True)
+                logging.info("Column ProductNumber successfully renamed to ItemNumber")
+            if 'SupplyingWarehouse' in self.data.columns():
+                self.data.rename(columns={'SupplyingWarehouse': 'Warehouse'}, inplace=True)
+                logging.info("Column SupplyingWarehouse successfully renamed to Warehouse")
+            if 'CountryOfOrigin' in self.data.columns():
+                self.data.rename(columns={'CountryOfOrigin': 'SalesMarket'}, inplace=True)
+                logging.info("Column CountryOfOrigin successfully renamed to SalesMarket")
+        except Exception as e:
+            logging.error(
+                f"Error trying to rename columns ProductNumber, SupplyingWarehouse, \
+                CountryOfOrigin: {e}")
+    
+    def imputation_salesmarket_warehouse(self):
+        try:
+            if self.data['SalesMarket'].isnull().any():
+                self.data['SalesMarket'].fillna('MISSING', inplace=True)
+                logging.info("Missing values in SalesMarket column filled with 'MISSING'")
+            if self.data['Warehouse'].isnull().any():
+                self.data['Warehouse'].fillna('MISSING', inplace=True)
+                logging.info("Missing values in Warehouse column filled with 'MISSING'")
+        except Exception as e:
+            logging.error(f"Error filling missing values in SalesMarket, Warehouse column: {e}")
